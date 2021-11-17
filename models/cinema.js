2eebafe7f042cc8edbbd31175e6f6724d8e5ad09
Craig Marshall <dev.craigmarshall@gmail.com>
2021-11-17 13:32:31 +0000

    Completed a list of titles and find film by title test

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,26 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.filmTitles = function(){
+  const filmTitles = []
+  this.films.forEach((film) => {
+    filmTitles.push(film.title)
+  })
+  return filmTitles
+};
+
+Cinema.prototype.findByTitle = function(filmTitle) {
+    const result = this.films.find((film) => {
+      return film.title === filmTitle;
+    });
+    return result;
+  };
+
+// Cinema.prototype.findByTitle = function(filmTitle) {
+//   const result = this.films.filter((film) => {
+//     return film.title === filmTitle;
+//   });
+//   return result;
+// };
+
 module.exports = Cinema;
